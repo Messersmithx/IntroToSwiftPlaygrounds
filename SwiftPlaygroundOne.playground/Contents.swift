c6d1f5fcfbb2a78ad60370602e4356f674bd472b
Messersmithx <59902281+Messersmithx@users.noreply.github.com>
2020-01-21 10:46:45 -1000

    Part 9
    wahoo

--- SwiftPlaygroundOne.playground/Contents.swift
+++ SwiftPlaygroundOne.playground/Contents.swift
@@ -4,9 +4,20 @@ var str = "Hello, playground";
 
 var myFirstVariable =  "I created my first string ";
 
-//Declare and assign varible in single line of code
 var mySecondVariable = "and look I addded two strings together ";
 
 var myThirdVariable = myFirstVariable + mySecondVariable;
 
-print(myThirdVariable)
+    print(myThirdVariable)
+
+var tacoTime = "It is taco time!"
+
+var theMoonMan = "I am the moon!"
+
+var wow = "Hello! you wow me!"
+
+var space = " "
+
+var combinedStrings = tacoTime + space + theMoonMan + space + wow
+
+print(combinedStrings)
