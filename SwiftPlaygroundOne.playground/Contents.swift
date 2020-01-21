ce6e082be4b0f58b6a31557f90b742f99e8e1926
Messersmithx <59902281+Messersmithx@users.noreply.github.com>
2020-01-21 10:33:12 -1000

    Updated to section 7 in class
    Wahoo!

--- SwiftPlaygroundOne.playground/Contents.swift
+++ SwiftPlaygroundOne.playground/Contents.swift
@@ -1,3 +1,12 @@
 import UIKit
 
-var str = "Hello, playground"
+var str = "Hello, playground";
+
+var myFirstVariable =  "I created my first string ";
+
+//Declare and assign varible in single line of code
+var mySecondVariable = "and look I addded two strings together ";
+
+var myThirdVariable = myFirstVariable + mySecondVariable;
+
+print(myThirdVariable)
