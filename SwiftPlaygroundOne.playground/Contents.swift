81be7ad8e2c507392575b7bfe62e57b4c0aa861a
Messersmithx <59902281+Messersmithx@users.noreply.github.com>
2020-01-23 10:20:32 -1000

    Part 9
    Wahoo!

--- SwiftPlaygroundOne.playground/Contents.swift
+++ SwiftPlaygroundOne.playground/Contents.swift
@@ -21,3 +21,38 @@ var space = " "
 var combinedStrings = tacoTime + space + theMoonMan + space + wow
 
 print(combinedStrings)
+
+ /* Assignment two */
+
+ var appState = false
+
+var firstInt = 1
+
+var secondInt = 2
+
+print("App State is equal to: ")
+
+print(appState)
+
+
+
+
+
+var newVarCompared = firstInt < secondInt
+
+var termOne = "taco"
+
+var termTwo = "taco"
+
+var termCompared = termOne  ==  termTwo
+
+
+
+
+
+
+if (termTwo == termOne) {
+    print("Terms are the same value")
+} else {
+    print("Terms are not the same value")
+}
